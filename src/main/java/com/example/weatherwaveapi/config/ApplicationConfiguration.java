af7c305587337e1c27f8f81a341d7f3c0c321366
Anna Sargsyan <ansargsyan@griddynamics.com>
2024-01-30 12:34:21 +0400

    TOOL-10-implemnt AppConfig

---
+++ src/main/java/com/example/weatherwaveapi/config/ApplicationConfiguration.java
@@ -0,0 +1,25 @@
+package com.example.weatherwaveapi.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+import java.time.Duration;
+
+@Configuration
+public class ApplicationConfiguration {
+    @Value("${connectionTimeout}")
+    private int connectionTimeout;
+    @Value("${readTimeout}")
+    private int readTimeout;
+
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder
+                .setConnectTimeout(Duration.ofMillis(connectionTimeout))
+                .setReadTimeout(Duration.ofMillis(readTimeout))
+                .build();
+    }
+}
