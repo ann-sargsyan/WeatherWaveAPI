6617cc41dd35eeb7b350f1da460ee00224091b00
ahakobyan <ahakobyan@griddynamics.com>
2024-02-01 12:53:32 +0400

    TOOL-13-general settings impl

--- src/main/java/com/example/weatherwaveapi/WeatherWaveApiApplication.java
+++ src/main/java/com/example/weatherwaveapi/WeatherWaveApiApplication.java
@@ -1,9 +1,12 @@
 package com.example.weatherwaveapi;
 
+import com.example.weatherwaveapi.config.GeneralSettings;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(value = {GeneralSettings.class})
 public class WeatherWaveApiApplication {
     public static void main(String[] args) {
         SpringApplication.run(WeatherWaveApiApplication.class, args);
