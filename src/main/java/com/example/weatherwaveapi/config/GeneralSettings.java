05f049a7a91ef2e008560aec290a0895e18ecad3
ahakobyan <ahakobyan@griddynamics.com>
2024-02-01 19:24:47 +0400

    TOOL-13-update general settings impl based on review

--- src/main/java/com/example/weatherwaveapi/config/GeneralSettings.java
+++ src/main/java/com/example/weatherwaveapi/config/GeneralSettings.java
@@ -7,8 +7,38 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @ConfigurationProperties(prefix = "application.settings")
 public class GeneralSettings {
 
-    private OpenWeatherApi openWeatherApi;
-    private YandexApi yandexApi;
-    private RestTemplateProperties restTemplateProperties;
+    private OpenWeather openWeatherMap;
+    private Yandex yandexMap;
+    private RestTemplate restTemplateProperties;
+
+    @Data
+    public static class OpenWeather {
+        private Api api;
+
+        @Data
+        public static class Api {
+            private String key;
+            private String weatherUrl;
+            private String forecastUrl;
+        }
+    }
+
+    @Data
+    public static class Yandex {
+        private Api api;
+
+        @Data
+        public static class Api {
+            private String key;
+            private String url;
+        }
+    }
+
+    @Data
+    public static class RestTemplate {
+        private Integer readTimeout;
+        private Integer connectionTimeout;
+    }
+
 }
 
