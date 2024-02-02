0b2bab110f20ad9d819f1bd24128eee1396b480c
ahakobyan <ahakobyan@griddynamics.com>
2024-02-02 18:01:56 +0400

    TOOL-13-separate GeneralSettings, improve OpenWeatherUrlBuilder based on reviews

---
+++ src/main/java/com/example/weatherwaveapi/util/urlbuilder/OpenWeatherUrlBuilder.java
@@ -0,0 +1,61 @@
+package com.example.weatherwaveapi.util.urlbuilder;
+
+import com.example.weatherwaveapi.config.GeneralSettings;
+import jakarta.ws.rs.core.UriBuilder;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+import static com.example.weatherwaveapi.util.urlbuilder.ParameterList.*;
+
+
+@Component
+@RequiredArgsConstructor
+public class OpenWeatherUrlBuilder {
+
+    private final GeneralSettings generalSettings;
+
+    public String buildWeatherUrl(String query) {
+        return buildUrl(weatherApiUrl(), Map.of(QUERY_PARAM, query));
+    }
+
+    public String buildForecastUrlForCity(String city) {
+        return buildUrl(forecastApiUrl(), Map.of(QUERY_PARAM, city));
+    }
+
+    public String buildWeatherUrlForZipcode(Integer zipcode, String country) {
+        return UriBuilder.fromUri(weatherApiUrl())
+                .queryParam(API_KEY_PARAM, apiKey())
+                .queryParam(ZIP_PARAM, zipcode, country)
+                .build()
+                .toString();
+    }
+
+    public String buildWeatherUrlForCoord(double lat, double lon) {
+        return buildUrl(weatherApiUrl(), Map.of(LAT_PARAM, lat, LON_PARAM, lon));
+    }
+
+    private String buildUrl(String apiUrl, Map<String, Object> queryParams) {
+        UriBuilder uriBuilder = UriBuilder
+                .fromUri(apiUrl)
+                .queryParam(API_KEY_PARAM, apiKey());
+
+        queryParams.forEach(uriBuilder::queryParam);
+
+        return uriBuilder.build().toString();
+    }
+
+    private String weatherApiUrl() {
+        return generalSettings.getOpenWeatherApi().weatherUrl();
+    }
+
+    private String forecastApiUrl() {
+        return generalSettings.getOpenWeatherApi().forecastUrl();
+    }
+
+    private String apiKey() {
+        return generalSettings.getOpenWeatherApi().key();
+    }
+
+}
