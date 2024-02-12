696538f39f217876f33b9fb7c908b7215abe9765
ahakobyan <ahakobyan@griddynamics.com>
2024-02-12 15:42:25 +0400

    Yandex test

--- src/main/java/com/example/weatherwaveapi/WeatherWaveApiApplication.java
+++ src/main/java/com/example/weatherwaveapi/WeatherWaveApiApplication.java
@@ -2,8 +2,9 @@ package com.example.weatherwaveapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class WeatherWaveApiApplication {
     public static void main(String[] args) {
         SpringApplication.run(WeatherWaveApiApplication.class, args);
--- src/main/java/com/example/weatherwaveapi/controller/WeatherController.java
+++ src/main/java/com/example/weatherwaveapi/controller/WeatherController.java
@@ -2,6 +2,7 @@ package com.example.weatherwaveapi.controller;
 
 import com.example.weatherwaveapi.model.request.WeatherRequest;
 import com.example.weatherwaveapi.model.response.WeatherResponse;
+import com.example.weatherwaveapi.service.ServiceImpl;
 import com.example.weatherwaveapi.service.WeatherService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -19,6 +20,7 @@ public class WeatherController {
     private static final String REQUEST_MESSAGE = "Request contain serviceApiEnumValue: {}";
 
     private final WeatherService weatherService;
+    private final ServiceImpl service;
 
     @GetMapping(value = "/city", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<WeatherResponse> getWeatherInSelectedCity(@RequestParam String city) {
@@ -38,5 +40,12 @@ public class WeatherController {
         log.info(REQUEST_MESSAGE, weatherRequest);
         WeatherResponse response = weatherService.getWeather(weatherRequest);
         return ResponseEntity.ok(response);
+    }
+
+    @GetMapping(value = "yandex")
+    public ResponseEntity<Object> getYandex() {
+        log.info(REQUEST_MESSAGE);
+        System.out.println(service.getWeather());
+        return ResponseEntity.ok(service.getWeather());
     }
 }
