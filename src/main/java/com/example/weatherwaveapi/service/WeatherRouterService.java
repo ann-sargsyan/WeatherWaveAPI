fcc337417cb59602cf0093d6328f678c71b7d31e
ann-sargsyan <156207438+ann-sargsyan@users.noreply.github.com>
2024-02-16 17:19:44 +0400

    Make change in WeatherRouterService

--- src/main/java/com/example/weatherwaveapi/service/WeatherRouterService.java
+++ src/main/java/com/example/weatherwaveapi/service/WeatherRouterService.java
@@ -1,25 +1,22 @@
 package com.example.weatherwaveapi.service;
 
-import com.example.weatherwaveapi.config.GeneralSettings;
 import com.example.weatherwaveapi.serviceapienum.ServiceApiEnum;
-import com.example.weatherwaveapi.util.urlbuilder.UrlBuilderForWeather;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 @Service
 @RequiredArgsConstructor
-public final class WeatherRouterService {
+public class WeatherRouterService {
+    private static final String NOT_SUPPORTED_MESSAGE = "Not supported service";
+    private final OpenWeatherService openWeatherService;
+    private final YandexWeatherService yandexWeatherService;
 
-    private final UrlBuilderForWeather urlBuilderForWeather;
-    private final GeneralSettings generalSettings;
-    private final RestTemplate restTemplate;
 
     public WeatherService getWeatherBySelectedService(ServiceApiEnum service) {
         return switch (service) {
-            case OPEN_WEATHER_MAP -> new OpenWeatherService(restTemplate, urlBuilderForWeather);
-            case YANDEX -> new YandexWeatherService(restTemplate, generalSettings, urlBuilderForWeather);
-            default -> null;
+            case OPEN_WEATHER_MAP -> openWeatherService;
+            case YANDEX -> yandexWeatherService;
+            default -> throw new IllegalArgumentException(NOT_SUPPORTED_MESSAGE);
         };
     }
 }
